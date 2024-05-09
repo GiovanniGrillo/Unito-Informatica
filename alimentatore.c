76fbfc67baaceb8eac4988cf1ed795f780f2d5ef
fra <fra@email.com>
2024-05-09 15:53:18 +0200

    Co-authored-by: GiovanniGrillo <GiovanniGrillo@users.noreply.github.com>

--- alimentatore.c
+++ alimentatore.c
@@ -3,9 +3,10 @@
 
 
 int main() {
-    printf("\n\033[1;32mBenvenuto in alimentatore!\033[0m\n");
+    // printf("\n\033[1;32mBenvenuto in alimentatore!\033[0m\n");
     setbuf(stdout, NULL);
     loadIPCs();
+    signal(SIGINT, handle_sigint);
     struct timespec req = {0,var->STEP_ALIMENTAZIONE};
 
     while(var->flagTerminazione != 1) {
@@ -13,7 +14,7 @@ int main() {
         attShm();
         {
             if (creazione_atomi(var->N_NUOVI_ATOMI) == -1) ERROR;
-            printf("\n\033[1;32mHEY SONO ALIMENTATORE HO CREATO %d nuovi atomi fino ad adesso sono : %d ---------------------------------\033[0m\n",var->N_NUOVI_ATOMI, centrale->n_atomi);
+             printf("\n\033[1;32mHEY SONO ALIMENTATORE HO CREATO %d nuovi atomi fino ad adesso sono : %d ---------------------------------\033[0m\n",var->N_NUOVI_ATOMI, centrale->n_atomi);
         }
         dettShm();
         releaseSem(semProcessi, 0);
