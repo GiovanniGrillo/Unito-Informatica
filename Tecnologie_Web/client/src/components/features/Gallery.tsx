4dec3db00cf8df2a332e7ecdacb93b75666a6eb9
GiovanniGrillo <grillogiovanni123@gmail.com>
2025-03-17 16:58:36 +0100

    TODO list updated. HomePage bottom galley names changed.

--- Tecnologie_Web/client/src/components/features/Gallery.tsx
+++ Tecnologie_Web/client/src/components/features/Gallery.tsx
@@ -9,14 +9,14 @@ interface GalleryImage {
 
 const Gallery: React.FC = () => {
   const galleryImages: GalleryImage[] = [
-    { src: '/img/gallery/gallery_1.png', alt: 'Gallery  1' },
-    { src: '/img/gallery/gallery_2.png', alt: 'Gallery Image 2' },
-    { src: '/img/gallery/gallery_3.png', alt: 'Gallery Image 3' },
-    { src: '/img/gallery/gallery_4.png', alt: 'Gallery Image 4' },
-    { src: '/img/gallery/gallery_5.png', alt: 'Gallery Image 5' },
-    { src: '/img/gallery/gallery_6.png', alt: 'Gallery Image 6' },
-    { src: '/img/gallery/gallery_7.png', alt: 'Gallery Image 7' },
-    { src: '/img/gallery/gallery_8.png', alt: 'Gallery Image 8' }
+    { src: '/img/gallery/gallery_1.png', alt: 'Sala tagli' },
+    { src: '/img/gallery/gallery_2.png', alt: 'Reception' },
+    { src: '/img/gallery/gallery_3.png', alt: 'Sala VIP' },
+    { src: '/img/gallery/gallery_4.png', alt: 'Area attesa' },
+    { src: '/img/gallery/gallery_5.png', alt: 'Sala tagli' },
+    { src: '/img/gallery/gallery_6.png', alt: 'Vetrina prodotti' },
+    { src: '/img/gallery/gallery_7.png', alt: 'Sala lavaggio' },
+    { src: '/img/gallery/gallery_8.png', alt: 'Sala styling' }
   ];
 
   return (
