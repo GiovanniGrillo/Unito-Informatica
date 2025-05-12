ba97db0c0bd67db3aa8f5e09c0ac7895114fa12b
GiovanniGrillo <grillogiovanni123@gmail.com>
2025-05-12 22:23:04 +0200

    NumberTok implementation. It works, not sure it's correct

--- Linguaggi_Formali_e_Traduttori/ex5/5.1/NumberTok.java
+++ Linguaggi_Formali_e_Traduttori/ex5/5.1/NumberTok.java
@@ -1,12 +1,7 @@
 public class NumberTok extends Token {
-    public int num;
-    
-    public NumberTok(int tag, int n) {
-        super(tag);
-        num = n;
-    }
-    
-    public String toString() {
-        return "<" + tag + ", " + num + ">";
-    }
+	public int lexeme;
+    public NumberTok(int tag, String s) { super(tag); lexeme=Integer.parseInt(s); }
+    public NumberTok(int tag, int n) { super(tag); lexeme=n; }
+    public String toString() { return "<" + tag + ", " + lexeme + ">"; }
 }
+//non sono sicuro di questa parte da rivisitare
