eb5f90d7386f97fb499fadd881f79b8ace0932bd
GiovanniGrillo <grillogiovanni123@gmail.com>
2025-06-03 14:51:39 +0200

    numbertok included in makefile

--- Linguaggi_Formali_e_Traduttori/ex2/2.1/NumberTok.java
+++ Linguaggi_Formali_e_Traduttori/ex2/2.1/NumberTok.java
@@ -1,11 +1,11 @@
 public class NumberTok extends Token {
     public int num;
-    
+
     public NumberTok(int tag, int n) {
         super(tag);
         num = n;
     }
-    
+
     public String toString() {
         return "<" + tag + ", " + num + ">";
     }
